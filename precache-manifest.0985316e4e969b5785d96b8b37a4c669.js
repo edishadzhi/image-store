e5205f68ce77ab46ce3da4466b07e430aa5f41c5
edishadzhi <edishadzhi@gmail.com>
2021-05-11 20:20:14 +0300

    Updates

---
+++ precache-manifest.0985316e4e969b5785d96b8b37a4c669.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "108c3af4b1c1669a734be657257626f9",
+    "url": "/image-store.git/index.html"
+  },
+  {
+    "revision": "628f3a2d5f7c982f5452",
+    "url": "/image-store.git/static/css/main.60c55136.chunk.css"
+  },
+  {
+    "revision": "0b369d42d3c14e87a440",
+    "url": "/image-store.git/static/js/2.d76d15b4.chunk.js"
+  },
+  {
+    "revision": "595b10b6cbc84b55f65f92de9a4f48b7",
+    "url": "/image-store.git/static/js/2.d76d15b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "628f3a2d5f7c982f5452",
+    "url": "/image-store.git/static/js/main.ee116740.chunk.js"
+  },
+  {
+    "revision": "e145e027f88519f76c60",
+    "url": "/image-store.git/static/js/runtime-main.a737b99c.js"
+  }
+]);
